7a2fba06c2baba0decf8303f7c81b49fb8e14b43
santfirax <santi-1524@hotmail.com>
2018-01-19 15:51:30 -0500

    first commit

---
+++ src/main/java/co/com/tns/conversor/ConvertirHtml.java
@@ -0,0 +1,116 @@
+package co.com.tns.conversor;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ConvertirHtml {
+
+	public List<String> convertir(String string) {
+		// TODO Auto-generated method stub
+		String cadenaEspacio = string.substring(0, 1);
+		if(!cadenaEspacio.equals(" ")) {
+			return Arrays.stream(string.split(" ")).collect(Collectors.toList());
+		}
+		else {
+			return null;
+		}
+	}
+
+//	public boolean validarTamañoDeLaCadena(String header) {
+//		// TODO Auto-generated method stub
+//		return header.length()<7;
+//	}
+//
+//	public int contarNumeroDeNumerales(String header) {
+//		// TODO Auto-generated method stub
+	
+
+	public boolean validarTamañoDeLaCadena(String header) {
+		// TODO Auto-generated method stub
+		return header.length()<7;
+	}
+
+	public boolean contarNumeroDeNumerales(String header) {
+		// TODO Auto-generated method stub
+		
+		char[] arreglonumerales = header.toCharArray();
+		for(int i=0;i<arreglonumerales.length;i++)
+		{
+			if(arreglonumerales[i]=='#')
+			{
+			
+			}
+			else
+			{
+				return false;
+			}
+				
+			
+			
+		}
+		
+		return true;
+	}
+
+	public int MostrarEtiqueta(String header) {
+		// TODO Auto-generated method stub
+		int  contador=0;
+		char[] arreglonumerales = header.toCharArray();
+		for(int i=0;i<arreglonumerales.length;i++)
+		{
+			if(arreglonumerales[i]=='#')
+			{
+				contador++;
+			}
+			else
+			{
+				break;
+			}
+				
+			
+			
+		}
+		
+		switch(contador) {
+		case 1: header = "<H1>" + header + "</H1>";
+		        header = header.replaceAll("# ", "");
+				System.out.println(header);
+				break;
+		case 2: header = "<H2>" + header + "</H2>";
+		header = header.replaceAll("## ", "");
+		System.out.println(header);
+		break;
+		case 3: header = "<H3>" + header + "</H3>";
+		header = header.replaceAll("### ", "");
+		System.out.println(header);
+		break;
+		case 4: header = "<H4>" + header + "</H4>";
+		header = header.replaceAll("#### ", "");
+		System.out.println(header);
+		break;
+		case 5: header = "<H5>" + header + "</H5>";
+		header = header.replaceAll("##### ", "");
+		System.out.println(header);
+		break;
+		case 6: header = "<H6>" + header + "</H6>";
+		header = header.replaceAll("####### ", "");
+		System.out.println(header);
+		break;
+		case 7: 
+		System.out.println(header);
+		break;
+		}
+		return 1;
+	
+		
+		
+	}
+
+	
+
+	
+		
+
+}
---
+++ src/test/java/co/com/tns/conversor/ConversorTest.java
@@ -0,0 +1,65 @@
+package co.com.tns.conversor;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class ConversorTest {
+	/*@Test
+	public void debeConvertirCadenaEnLista()
+	{
+		
+		//Arrange, inicializo
+		ConvertirHtml convertirHtml = new ConvertirHtml();
+		//Act
+		List<String> header = convertirHtml.convertir("# Hola");
+		//Assert
+		Assert.assertTrue((null != convertirHtml)&&(header.size()>1));
+		
+		
+	}*/
+	@Test
+	public void debeValidarSiElPrimerCaracterNoSeaEspacio(){
+		//Arrange, inicializo
+				ConvertirHtml convertirHtml = new ConvertirHtml();
+				//Act
+				List<String> header = convertirHtml.convertir("# Hola");
+				//Assert
+				Assert.assertTrue((header != null) && (header.size() > 1));
+		
+	}
+	@Test
+	public void debeValidarTamañoDeLaCadena() {
+		//Arrange
+		ConvertirHtml convertirHtml = new ConvertirHtml();
+		//act
+		String header = "######";
+		//assert
+		Assert.assertTrue(convertirHtml.validarTamañoDeLaCadena(header));
+		
+		
+		
+	}
+	@Test
+	public void debeContarNumeroDeNumerales() {
+		//Arrange
+		ConvertirHtml convertirHtml = new ConvertirHtml();
+		//act
+		String header = "##";
+		//assert
+		Assert.assertTrue(convertirHtml.contarNumeroDeNumerales(header));
+	}
+	@Test
+	public void debeMostrarEtiqueta() {
+		//Arrange
+		ConvertirHtml convertirHtml = new ConvertirHtml();
+		//act
+		String header = "#######Hackaton";
+		//assert
+		Assert.assertTrue(1==convertirHtml.MostrarEtiqueta(header));
+	}
+	
+	
+
+}
